3c8324dff5162e9f4b4b38264bc679f2c1bb8fbe
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2024-11-21 11:06:35 -0600

    add comments and clean up

--- src/machine.py
+++ src/machine.py
@@ -2,8 +2,6 @@ from ingredients import Ingredient, IngredientType, MAPPINGS
 
 class Machine:
     """Represents our machine (NFA)"""
-    def __init__(self):
-        pass
     
     def parse(self, input: str) -> list[Ingredient]:
         """Parses an input string into a list of ingredients in order"""
@@ -13,6 +11,7 @@ class Machine:
         return _
     
     def run(self, input: str):
+        """Runs the machine with a given input string"""
         # Parse the input
         ingredients: list[Ingredient] = self.parse(input)
         
@@ -21,25 +20,23 @@ class Machine:
             print("Invalid input. Please enter at least one ingredient.")
             return
         
-        # TRANSITION 1: Check if the first ingredient is a pasta
+        # STATE 1: Check if the first ingredient is a pasta
         if ingredients[0].type == IngredientType.PASTA:
             print(f"1. Begin by cooking the {ingredients[0]} pasta noodles.")
         else:
             print("Invalid input. Please enter a valid pasta.")
             return
         
-        # TRANSITION(s) 2: Check for sauces and addons
+        # STATE(s) 2: Check for sauces and addons
         for i, ingredient in enumerate(ingredients[1:], 2):
             match (ingredient.type):
                 case IngredientType.SAUCE:
                     print(f"{i}. Add {ingredient} sauce to the pasta.")
-                    break
                 case IngredientType.ADDON:
                     print(f"{i}. Add {ingredient} to the pasta.")
-                    break
                 case _:
                     print(f"Invalid input. Please enter a valid sauce or addon.")
                     return
-        
-        
-        
+                
+        # END STATE
+        print("Your pasta dish is ready! Enjoy!")
