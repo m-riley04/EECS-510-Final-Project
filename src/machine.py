3f71fb89996fb09ebd09c5fa86f8e2894202bbc5
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2024-11-21 10:40:27 -0600

    add basic implementation of machine flow

--- src/machine.py
+++ src/machine.py
@@ -1,16 +1,42 @@
-from ingredient import Ingredient
+from ingredients import Ingredient, IngredientType
 
 class Machine:
     """Represents our machine (NFA)"""
     def __init__(self):
         pass
     
-    def parse(self, input):
+    def parse(self, input: str) -> list[Ingredient]:
         # TODO: Implement parsing from string into array
         return input
     
-    def run(self, input: str) -> str:
+    def run(self, input: str):
         # Parse the input
-        parsed = self.parse(input)
+        ingredients: list[Ingredient] = self.parse(input)
+        
+        # Check if there is any input at all
+        if len(ingredients) <= 0:
+            print("Invalid input. Please enter at least one ingredient.")
+            return
+        
+        # TRANSITION 1: Check if the first ingredient is a pasta
+        if ingredients[0].type == IngredientType.PASTA:
+            print(f"1. Begin by cooking the {ingredients[0]} pasta noodles.")
+        else:
+            print("Invalid input. Please enter a valid pasta.")
+            return
+        
+        # TRANSITION(s) 2: Check for sauces and addons
+        for i, ingredient in enumerate(ingredients[1:], 2):
+            match (ingredient.type):
+                case IngredientType.SAUCE:
+                    print(f"{i}. Add {ingredient} sauce to the pasta.")
+                    break
+                case IngredientType.ADDON:
+                    print(f"{i}. Add {ingredient} to the pasta.")
+                    break
+                case _:
+                    print(f"Invalid input. Please enter a valid sauce or addon.")
+                    return
+        
         
         
