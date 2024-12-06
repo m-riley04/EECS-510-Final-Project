6df322d131066b2132517de0ccdacfb3e4d9bfe8
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2024-12-05 20:03:12 -0600

    remove unused stuff

--- src/state.py
+++ src/state.py
@@ -1,16 +1,6 @@
 class State:
     def __init__(self, name):
         self.name: str = name
-        self.inbound: set[State] = {}
-        self.outbound: set[State] = {}
-        
-    def add_transition(self, symbol, to_state):
-        if symbol not in self.outbound:
-            self.outbound[symbol] = set()
-        self.outbound[symbol].add(to_state)
-        if to_state not in self.inbound:
-            self.inbound[to_state] = set()
-        self.inbound[to_state].add(self)
         
     def __str__(self):
         return self.name
