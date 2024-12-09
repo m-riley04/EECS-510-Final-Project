ac6754dec1e43f0c3173938bdf6e44a3f4930440
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-12-08 20:22:07 -0600

    add formatted printing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from automata.pasta_nfa import PastaNFA
 
 ### GLOBAL CONSTANTS ###
-DEBUG: bool         = False
-NFA_FILENAME: str   = "machines/nfa.txt"
+DEBUG: bool                 = False
+FORMATTED_STEPS: bool       = True
+NFA_FILENAME: str           = "machines/nfa.txt"
 
 def accept(A: PastaNFA, w: str) -> None:
     """
@@ -10,20 +11,20 @@ def accept(A: PastaNFA, w: str) -> None:
     Prints either "accept" with the path/trace, or "reject".
     """
     # Run the input in the machine
-    _ = A.accept(w)
+    _ = A.accept(w=w)
     
-    # Print the acceptance result
+    # Print the acceptance result and path (if accepted)
     print(_)
-
-    # Print the path if accepted
     if bool(_) == True: 
         for t in _.path:
             print(t)
 
+    # Print the formatted steps
+    if FORMATTED_STEPS: A.print_formatted_steps(_)
+
 def main():
     # Initialize NFA
     nfa: PastaNFA = PastaNFA(filename=NFA_FILENAME, debug=DEBUG)
-    nfa.print() # Display parsed NFA info
     
     # Get the input string
     inputString = input("Enter your ingredients: ")
